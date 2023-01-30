7afcace5f3acf30684cef60be94ae91436fc3e4d
Erwan Delcour <123735203+Horganon@users.noreply.github.com>
2023-01-30 14:44:47 +0100

    add correction for githubPages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Switch, Routes, Route } from 'react-router-dom';
 import Menu from './components/menu';
 import LandData from './components/landData'
 
@@ -7,12 +7,10 @@ import LandData from './components/landData'
 
 function App() {
   	return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Menu/>}/>
-                <Route path="/landData/:id/:date_start/:date_end" element={<LandData/>} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Menu/>} />
+            <Route path="/landData/:id/:date_start/:date_end" element={<LandData/>} />
+        </Routes>
     );
 }
 
